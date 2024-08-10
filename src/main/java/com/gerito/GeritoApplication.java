534c2eb349415961d919e74533a4e615a0c34eed
Juan Castano <>
2024-08-10 02:49:53 -0500

    first commit

---
+++ src/main/java/com/gerito/GeritoApplication.java
@@ -0,0 +1,13 @@
+package com.gerito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerito/GeritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
